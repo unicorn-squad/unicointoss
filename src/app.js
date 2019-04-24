4c3f990b576a0b9714d3fed6fa3b4847513218d5
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-04-24 15:53:03 -0700

    added text response to button click

--- src/app.js
+++ src/app.js
@@ -3,8 +3,11 @@ import getHornsOrButts from './get-horns-or-butts.js';
 //posting our cast list, and calling the elements we want
 const unicoinButton = document.getElementById('unicoin-button');
 const uniImage = document.getElementById('uni-image');
+const message = document.getElementById('message');
+const headsChoice = document.getElementById('heads-choice');
+
 //initialize stuff
-console.log(unicoinButton);
+
 
 //add event listener for the button click, wiring UP!
 unicoinButton.addEventListener('click', () => {
@@ -14,6 +17,25 @@ unicoinButton.addEventListener('click', () => {
 //change image display
     const imageSource = './assets/' + flip + '.jpg';
     uniImage.src = imageSource;
+
+//initializing empty guess, making it unihead or unibutt
+//linking our radio button guess to our unicoin button
+    let guess = '';
+    if(headsChoice.checked) {
+        guess = 'unihead';
+    } else {
+        guess = 'unibutt';
+    }
+
+//display message and change based on user selection
+    const guessedCorrect = flip === guess;
+
+    if(guessedCorrect) {
+        message.textContent = 'You won!';
+    }
+    else {
+        message.textContent = 'You lose!';
+    }
 
     
 });
